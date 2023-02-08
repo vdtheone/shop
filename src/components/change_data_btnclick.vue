0cc1e909da2caad09be422663f27c9e7b4e39a54
Vishal Prajapati <vp22052001@gmail.com>
2023-02-08 23:12:13 +0530

    change data on button click(@click event)

---
+++ src/components/change_data_btnclick.vue
@@ -0,0 +1,26 @@
+<template lang="">
+    <div>
+        <h1>{{val}}</h1>
+        <input type="text" v-model="newval" class="form-control mt-2 mb-2" placeholder="Enter Value for change the value of test">
+        <button class="btn btn-primary" @click="change_data"> change </button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            val:"test",
+            newval : ""
+        }
+    },
+    methods: {
+      change_data(){
+        this.val = this.newval;
+      }  
+    },
+}
+</script>
+<style lang="">
+    
+</style>
