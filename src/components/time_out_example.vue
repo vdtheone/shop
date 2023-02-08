7c83657ea090373c0f3579a2b174bcdf7c6864f8
Vishal Prajapati <vp22052001@gmail.com>
2023-02-08 23:09:54 +0530

    timeout function

---
+++ src/components/time_out_example.vue
@@ -0,0 +1,61 @@
+<template lang="">
+<div>
+    <!-- <h1>timeout</h1> -->
+    {{message}}
+    {{data}}
+</div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            // message: "Welcome, Please Wait...."
+            message: "",
+            data:'',
+        }
+    },
+    methods: {
+        
+        callFunction: function () {
+            var v = this;           
+            setTimeout(function () {
+
+                // v.message = "Hi Bro, SetTimeout is working fine.";
+                v.message = "Welcome";
+
+            }, 3000);
+
+        },
+        callFunction2: function () {
+            var p = this;           
+            setTimeout(function () {
+
+                // v.message = "Hi Bro, SetTimeout is working fine.";
+                p.data = "Bye Bye";
+
+            }, 2000);
+
+        }
+    },
+    // beforeMount(){
+    //     this.callFunction()
+    //     // alert("beforemount called")
+    //     console.log("beforemount called");
+    // },
+    mounted() {
+
+        this.callFunction()
+        
+        console.log("mount called");
+    },
+    unmounted(){
+        this.callFunction2()
+        console.log("unmounted");
+    }
+}
+</script>
+
+<style lang="">
+
+</style>
---
+++ src/components/time_out_main.vue
@@ -0,0 +1,26 @@
+<template lang="">
+    <div>
+        <h1>Time out main</h1>
+        <time_out_example />
+    </div>
+</template>
+
+<script>
+
+import time_out_example from './time_out_example.vue';
+
+export default {
+    data() {
+        return {
+            
+        }
+    },
+    components:{
+        time_out_example
+    }
+    
+}
+</script>
+<style lang="">
+    
+</style>
