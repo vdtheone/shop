702742d5ce145af39c8d9e005c58d5513a8dacf6
Vishal Prajapati <vp22052001@gmail.com>
2023-02-08 23:11:16 +0530

    togle show

---
+++ src/components/togle_show.vue
@@ -0,0 +1,22 @@
+<template lang="">
+    <div>
+        <h2 v-show="val">show & hide</h2>
+        <button class="btn btn-primary" @click="val=!val">click</button>
+    </div>
+</template>
+
+
+<script>
+export default {
+    data() {
+        return {
+            val:true,
+        }
+    },
+}
+</script>
+
+
+<style lang="">
+    
+</style>
