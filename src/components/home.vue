952a0fa8b9e6d8aac9cdd71be5fd85cfaa4678b8
Vishal Prajapati <vp22052001@gmail.com>
2023-02-08 23:18:16 +0530

    pass data from one component to other and example of Ref

---
+++ src/components/home.vue
@@ -0,0 +1,72 @@
+<template lang="">
+<div>
+    <!-- <child name="vishal" :getdata="getdata"/>
+    <h2>{{data}}</h2> -->
+
+    <ul>
+        <li v-for="item in users" ::key="item">
+            <!-- {{item.name}} {{item.email}} -->
+            <user :item="item" />
+        </li>
+    </ul>
+
+    <!-- example of ref -->
+    <!-- <div class="container w-50">
+        <input class="form-control" type="text" ref="input">
+        <button class="btn btn-primary mt-2 mb-2" v-on:click="getref()">click here for focus</button>
+    </div> -->
+
+</div>
+</template>
+
+<script>
+// import child from './child.vue';
+import user from './user.vue'
+export default {
+    name: "home",
+    data() {
+        return {
+            data: "",
+            users: [{
+                    name: "vishal",
+                    email: "vp@gmail.com"
+                },
+                {
+                    name: "vishal",
+                    email: "vp@gmail.com"
+                },
+                // {
+                //     name: "vishal",
+                //     email: "vp@gmail.com"
+                // },
+                // {
+                //     name: "vishal",
+                //     email: "vp@gmail.com"
+                // }
+            ]
+        }
+    },
+    components: {
+        // child,
+        user
+    },
+    methods: {
+        getdata(val) {
+            this.data = val;
+        },
+        // ref 
+        getref() {
+            // console.log("vishal")
+            this.$refs.input.focus();
+            this.$refs.input.style.color = "blue";
+            let val;
+            val = this.$refs.input.value;
+            console.log(val)
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
